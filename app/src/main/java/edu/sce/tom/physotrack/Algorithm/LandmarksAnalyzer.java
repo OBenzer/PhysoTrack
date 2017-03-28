3596b73481c37d5ba92dc56e661125890f550251
OBenz <OBenzer@gmail.com>
2017-03-28 12:50:03 +0300

    Adding methods for LandmarkAnalayzer (polygon area, distance calculation)

--- app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
+++ app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
@@ -1,5 +1,8 @@
 package edu.sce.tom.physotrack.Algorithm;
 
+import android.graphics.Point;
+import java.util.ArrayList;
+
 public class LandmarksAnalyzer {
     private final FaceLandmarks face;
 
@@ -11,4 +14,30 @@ public class LandmarksAnalyzer {
     {
         // Calc all the metrics of the face //
     }
+
+    //Calculate eye metrics, Send the side as the side of the eye needed//
+    private void caclEye(ArrayList<Point> points, char side) {
+
+    }
+
+    //Calculate the polygon area created by the list of points//
+    private float polygonAreaCalc(ArrayList<Point> points){
+        float area=0;
+        int numOfPoints = points.size();
+        for(int i=0; i<numOfPoints; i++){
+            area+=polygonTwoPointsCalc(points.get(i), points.get((i+1)%numOfPoints));
+        }
+        return Math.abs(area/2);
+    }
+
+    //Help method to calc two near points in the polygon points list//
+    private float polygonTwoPointsCalc(Point p1, Point p2){
+        return p1.x*p2.y-p1.y*p2.x;
+    }
+
+    //Calculate the distance between the two points given//
+    private float distanceCalc(Point p1, Point p2) {
+        return (float)Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+    }
+
 }
