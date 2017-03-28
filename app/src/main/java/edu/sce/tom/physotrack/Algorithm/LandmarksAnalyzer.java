1d43494cfa04e0142a67fc31586f927da8082af0
ydotco <ydotco@gmail.com>
2017-03-28 14:45:12 +0300

    added functions in algorithm

--- app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
+++ app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
@@ -1,10 +1,26 @@
 package edu.sce.tom.physotrack.Algorithm;
 
 import android.graphics.Point;
+import android.util.Log;
+
 import java.util.ArrayList;
 
 public class LandmarksAnalyzer {
     private final FaceLandmarks face;
+
+
+    private final char LEFT_SIDE='L';
+    private final char RIGHT_SIDE='R';
+
+    //metrics
+    private Point leftEyeCenter;
+    private Point rightEyeCenter;
+    private float leftEyeArea;
+    private float rightEyeArea;
+    private Point rightBrowCenter;
+    private Point leftBrowCenter;
+    private float leftEyeToBrowDistance;
+    private float rightEyeToBrowDistance;
 
     public LandmarksAnalyzer(FaceLandmarks f) {
         this.face = f;
@@ -13,7 +29,38 @@ public class LandmarksAnalyzer {
     public void analyzeFace()
     {
         // Calc all the metrics of the face //
+
     }
+
+    private void calcDistanceFromBrowToEye(Point a,Point b,char side){
+
+    }
+
+    private void calcEye(ArrayList<Point> eye,ArrayList<Point> brow,char side) {
+        // calculate the center of the eye
+        // calculate the area of the eye
+        //calculate the center of the eyebrow
+        //calculate distance from eyebrow to eye
+        Point eyeAvg = calcAvg(eye);
+        float eyeArea = calcPolygonArea(eye);
+        Point browAvg= calcAvg(brow);
+        float eyeToBrowDis=calcDistance(browAvg,eyeAvg);
+        switch (side) {
+            case RIGHT_SIDE:
+                rightEyeCenter = eyeAvg;
+                rightEyeArea = eyeArea;
+                rightBrowCenter=browAvg;
+                rightEyeToBrowDistance=eyeToBrowDis;
+            case LEFT_SIDE:
+                leftEyeCenter = eyeAvg;
+                leftEyeArea = eyeArea;
+                leftBrowCenter=browAvg;
+                leftEyeToBrowDistance=eyeToBrowDis;
+            default:
+                Log.d("debug", "calceye function in the default");
+        }
+    }
+
 
     private Point calcAvg(ArrayList<Point> land){
         int sumx=0;
