f30de9c5a6f1c56bdd31c7503ae90b30ea7c8c11
OBenz <OBenzer@gmail.com>
2017-03-28 13:07:03 +0300

    Adding methods PolygonArea Calculation

--- app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
+++ app/src/main/java/edu/sce/tom/physotrack/Algorithm/LandmarksAnalyzer.java
@@ -1,7 +1,6 @@
 package edu.sce.tom.physotrack.Algorithm;
 
 import android.graphics.Point;
-
 import java.util.ArrayList;
 
 public class LandmarksAnalyzer {
@@ -25,8 +24,19 @@ public class LandmarksAnalyzer {
         }
         int size=land.size();
         return new Point(sumx/size,sumy/size);
-
     }
 
+    private float calcPolygonArea(ArrayList<Point> points){
+        int numOfPoints = points.size();
+        float area=0;
+        for(int i=0; i<numOfPoints; i++)
+            area+=calcTwoPointsPolygon(points.get(i), points.get((i+1)%numOfPoints));
+
+        return Math.abs(area/2);
+    }
+
+    private float calcTwoPointsPolygon(Point p1, Point p2){
+        return p1.x*p2.y-p1.y*p2.x;
+    }
 
 }
