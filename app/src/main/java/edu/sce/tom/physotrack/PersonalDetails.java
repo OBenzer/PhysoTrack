69224788ea8e870d4f7725942963ec367fd12344
ydotco <ydotco@gmail.com>
2017-04-25 17:16:44 +0300

    added shared preferences in user details

--- app/src/main/java/edu/sce/tom/physotrack/PersonalDetails.java
+++ app/src/main/java/edu/sce/tom/physotrack/PersonalDetails.java
@@ -1,5 +1,7 @@
 package edu.sce.tom.physotrack;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,10 +11,21 @@ import android.widget.Toast;
 
 public class PersonalDetails extends AppCompatActivity {
 
+    //name of the shared preferences file and keys
+    private static final String USER_DETAILS_SP_FILE="userDetails";
+    public static final String USER_NAME = "userName";
+    public static final String SIDE = "defSide";
+    public static final String THERAPIST_MAIL = "therapistMail";
+
+
+    SharedPreferences userD;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_details);
+        userD = getSharedPreferences(USER_DETAILS_SP_FILE, Context.MODE_PRIVATE);
+        editor=userD.edit();
     }
 
     public void SubmitClicked(View view) {
@@ -28,12 +41,21 @@ public class PersonalDetails extends AppCompatActivity {
             SideSelcted = "Right";
         }
 
-        // just for checking
-        Toast.makeText(this, SideSelcted + " Side Selcted" , Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, user_name.getText().toString(), Toast.LENGTH_SHORT).show();
 
         // Enter All Information to DB
-
+        saveData(user_name.getText().toString(),SideSelcted,therapist_mail.getText().toString());
 
     }
+
+    private void saveData(String userName,String side,String therapistMail) {
+        editor.putString(USER_NAME,userName);
+        editor.putString(SIDE,side);
+        editor.putString(THERAPIST_MAIL,therapistMail);
+        editor.commit();
+        Toast.makeText(this, "Details entered successfully!", Toast.LENGTH_SHORT).show();
+        finish();
+
+    }
+
+
 }
