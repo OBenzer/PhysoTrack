59a742b956b43f1e3229da0b2dfa55c3146b29a7
yoninonasade <yoninonasade@gmail.com>
2017-04-29 14:52:06 +0300

    New Session LTR Fixed New Session Warnings Fixed Personal Details LTR Fixed ButtonShape Warnings Fixed Medical Information Layout Fixed Many spelling mistakes solved

--- app/src/main/java/edu/sce/tom/physotrack/youtube_information.java
+++ app/src/main/java/edu/sce/tom/physotrack/youtube_information.java
@@ -16,7 +16,7 @@ public class youtube_information extends AppCompatActivity {
         VideoView informationVideoView = (VideoView)findViewById(R.id.informationVideoView);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(informationVideoView);
-        Uri uri = Uri.parse("rtsp://r4---sn-4g5edn7y.googlevideo.com/Cj0LENy73wIaNAndvmELfSg5aBMYDSANFC2TeONYMOCoAUIASARgsvjCyOazvctYigELbElydkV6UG4tX0UM/D4069C3A3CB28C57B2BD0D8EAEDF0AEFA8F90DFA.4747A9145BA5C4C7320CDBE8B90432AD57E3777B/yt6/1/video.3gp");
+        Uri uri = Uri.parse("rtsp://r4---sn-4g5edn7y.googlevideo.com/Cj0LENy73wIaNAndvmELfSg5aBMYDSANFC2TeONYMOCoAUIASARgsvjCyOazvctYigELbElydkV6UG4tX0UM/D4069C3A3CB24747A9145BA5C4C7320CDBE8B90432AD57E3777B8C57B2BD0D8EAEDF0AEFA8F90DFA./yt6/1/video.3gp");
         informationVideoView.setMediaController(mediaController);
         informationVideoView.setVideoURI(uri);
         informationVideoView.requestFocus();
