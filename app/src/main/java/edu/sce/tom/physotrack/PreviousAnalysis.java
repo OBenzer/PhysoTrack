aaa6d5c0b9c3ef895dce1ba90429a8bd72d7f737
veredE <ve203962352>
2017-05-04 13:12:45 +0200

    attach a file txt to pysiotrappist mail/[v.e]

--- app/src/main/java/edu/sce/tom/physotrack/PreviousAnalysis.java
+++ app/src/main/java/edu/sce/tom/physotrack/PreviousAnalysis.java
@@ -2,14 +2,19 @@ package edu.sce.tom.physotrack;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static edu.sce.tom.physotrack.MainActivity.THERAPIST_MAIL;
 
@@ -35,16 +40,27 @@ public class PreviousAnalysis extends AppCompatActivity {
 
 
 
-    public void btn_send_therapist_on_click(View v) {
+    public void btn_send_therapist_on_click(View v) throws IOException, InterruptedException {
         SharedPreferences pref = getApplicationContext().getSharedPreferences(MainActivity.USER_DETAILS_SP_FILE, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         String email=pref.getString(THERAPIST_MAIL,"");// getting String
-         Toast.makeText(this, email , Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("plain/text");
+
+
         intent.putExtra(Intent.EXTRA_EMAIL,new String[]{email});
-        intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+        //create a txt file and write blabla and send to pysiotrapist mail
+        String filename="analysis.txt";
+        File filelocation = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), filename);
+        Uri path = Uri.fromFile(filelocation);
+
+       intent.setType("plain/text");
+        FileWriter writer=new FileWriter(filelocation);
+        writer.append("blabla");
+        writer.close();
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Analysis");
         intent.putExtra(Intent.EXTRA_TEXT, "mail body");
+        intent.putExtra(Intent.EXTRA_STREAM, path);
         startActivity(Intent.createChooser(intent, ""));
+
     }
 }
