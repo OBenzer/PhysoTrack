8d52c1c86aabf91bf8da94542f19b1551a223cc5
yoninonasade <yoninonasade@gmail.com>
2017-05-14 11:50:03 +0300

    Fix youtube clip (again)

--- app/src/main/java/edu/sce/tom/physotrack/youtube_information.java
+++ app/src/main/java/edu/sce/tom/physotrack/youtube_information.java
@@ -12,10 +12,12 @@ public class youtube_information extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_information);
 
+
+        String uriString = "rtsp://r12---sn-5hnedne7.googlevideo.com/Cj0LENy73wIaNAl03DV76iuaLxMYDSANFC3hFhhZMOCoAUIASARg8-fo87Gq3IpZigELanpTMTdHbEYtaTAM/CE7D44329A56E59E0BB4CE558E8EB095EF5888F6.4C6ABFE312A1BE0E5F6F0657976B3602419FF8F3/yt6/1/video.3gp" ;
         VideoView videoView = (VideoView)findViewById(R.id.videoViewInfo);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
-        Uri uri = Uri.parse("rtsp://r16---sn-5hne6n7r.googlevideo.com/Cj0LENy73wIaNAndvmELfSg5aBMYDSANFC3DQw9ZMOCoAUIASARg1v-h-peYkodZigELdnJmNUFUSGVLQmsM/39293BD4441D54E1AF8A5CB64FD732B05C990CCC.C5EC6F366348639F868F3AF8BF53E8E982202712/yt6/1/video.3gp");
+        Uri uri = Uri.parse(uriString);
         videoView.setMediaController(mediaController);
         videoView.setVideoURI(uri);
         videoView.requestFocus();
@@ -23,7 +25,6 @@ public class youtube_information extends AppCompatActivity {
 
     }
 }
-
 
 
 
