641db1fdce770d38b74fb32215c6739fedfbc1c7
yoninonasade <yoninonasade@gmail.com>
2017-05-15 18:57:45 +0300

    medical information Adding text To Speech

--- app/src/main/java/edu/sce/tom/physotrack/Medical_information.java
+++ app/src/main/java/edu/sce/tom/physotrack/Medical_information.java
@@ -1,16 +1,43 @@
 package edu.sce.tom.physotrack;
 
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Medical_information extends AppCompatActivity {
+
+    TextToSpeech toSpeech;
+    int result ;
+    TextView textView;
+    String text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_information);
 
+        textView = (TextView)findViewById(R.id.txt_medical_information);
+        toSpeech = new TextToSpeech(Medical_information.this,new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if(status==TextToSpeech.SUCCESS)
+                {
+
+                    result=toSpeech.setLanguage(Locale.ENGLISH);
+                }
+                else{
+
+                    Toast.makeText(Medical_information.this, "Not Supported in Device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     // when btn_youtube_exercises clicked move to youtube_information activity
@@ -18,4 +45,37 @@ public class Medical_information extends AppCompatActivity {
         Intent i = new Intent(Medical_information.this, youtube_information.class);
         startActivity(i);
     }
+
+    public void TTS(View view){
+
+        switch (view.getId()){
+
+            case R.id.btn_medical_info_Reader:
+                if(result==TextToSpeech.LANG_MISSING_DATA || result==TextToSpeech.LANG_NOT_SUPPORTED)
+                {
+                    Toast.makeText(this, "NOT SUPPORTED IN SWITCH CASE", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    text = textView.getText().toString();
+                    toSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(toSpeech!=null){
+
+            toSpeech.stop();
+            toSpeech.shutdown();
+        }
+    }
+
+
+
+
+
 }
